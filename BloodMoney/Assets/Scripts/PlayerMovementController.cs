1e9e350d572ff1f1942c4ce91c373f6af31c6a78
Kaajolee <latoza959@gmail.com>
2024-09-24 19:32:44 +0300

    test

--- BloodMoney/Assets/Scripts/PlayerMovementController.cs
+++ BloodMoney/Assets/Scripts/PlayerMovementController.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
@@ -14,6 +11,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float moveSpeed;
 
+    [SerializeField]
+    private bool isMobileEnabled;
+
+    [SerializeField]
+    private float horitontalInput, verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,47 +26,88 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        ReadInputs();
+        MoveCharacter();
+        //RotateCharacter();
     }
+    public void ReadInputs()
+    {
+        horitontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+    public void MoveCharacter()
+    {
+        Vector2 moveDirection = new Vector2(horitontalInput, verticalInput) * moveSpeed * Time.deltaTime;
+
+        transform.Translate(moveDirection);
+    }
+    public void RotateCharacter()
+    {
+        if (horitontalInput != 0 || verticalInput != 0)
+        {
+            Vector2 moveDirection = new Vector2(horitontalInput, verticalInput);
+
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
+
+
+
+    //--------------------MOBILE_CONTROLS--------------------------------------------
     public void MoveUp()
     {
-        if (playerRB != null)
+        if (isMobileEnabled)
         {
-            playerRB.velocity = new Vector2(0,1) * moveSpeed;
-            Debug.Log("Moving up");
+            if (playerRB != null)
+            {
+                playerRB.velocity = new Vector2(0, 1) * moveSpeed;
+                Debug.Log("Moving up");
+            }
+            else
+                Debug.LogError("Player RigidBody component null");
         }
-        else
-            Debug.LogError("Player RigidBody component null");
     }
     public void MoveDown()
     {
-        if (playerRB != null)
+        if (isMobileEnabled)
         {
-            playerRB.velocity = new Vector2(0, -1) * moveSpeed;
-            Debug.Log("Moving down");
+            if (playerRB != null)
+            {
+                playerRB.velocity = new Vector2(0, -1) * moveSpeed;
+                Debug.Log("Moving down");
+            }
+            else
+                Debug.LogError("Player RigidBody component null");
         }
-        else
-            Debug.LogError("Player RigidBody component null");
     }
     public void MoveLeft()
     {
-        if (playerRB != null)
+        if (isMobileEnabled)
         {
-            playerRB.velocity = new Vector2(-1, 0) * moveSpeed;
-            Debug.Log("Moving left");
+            if (playerRB != null)
+            {
+                playerRB.velocity = new Vector2(-1, 0) * moveSpeed;
+                Debug.Log("Moving left");
+            }
+            else
+                Debug.LogError("Player RigidBody component null");
         }
-        else
-            Debug.LogError("Player RigidBody component null");
     }
     public void MoveRight()
     {
-        if (playerRB != null)
+        if (isMobileEnabled)
         {
-            playerRB.velocity = new Vector2(1, 0) * moveSpeed;
-            Debug.Log("Moving right");
+            if (playerRB != null)
+            {
+                playerRB.velocity = new Vector2(1, 0) * moveSpeed;
+                Debug.Log("Moving right");
+            }
+            else
+                Debug.LogError("Player RigidBody component null");
         }
-        else
-            Debug.LogError("Player RigidBody component null");
     }
 
 }
