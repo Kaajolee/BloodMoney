8c21795abed205f4b1b2a103f4a56cad8a54a26a
Karolis <latoza959@gmail.com>
2024-09-25 14:49:00 +0300

    Render pipeline + menu background

---
+++ BloodMoney/Assets/Scripts/EnemyScripts/EnemyAI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    NavMeshAgent agent;
+
+    [SerializeField]
+    private Transform playerTransform;
+
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTowardsPlayer();
+    }
+    public void MoveTowardsPlayer()
+    {
+        if (agent != null)
+        {
+            agent.Move(playerTransform.position);
+        }
+    }
+}
